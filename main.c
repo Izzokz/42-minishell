fe1467c687eafa9a4e77ab760bfccb18aa51d191
Kylian Zhen-Claverie <kzhen-cl@2F2.42angouleme.fr>
2025-01-11 12:43:46 +0100

    Test with readline

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
+
+int	main(int argc, char **argv, char **envp)
+{
+	char	*line;
+
+	(void) argc;
+	(void) argv;
+	(void) envp;
+	line = readline("minishell > ");
+	while (line)
+	{
+		free(line);
+		line = readline("minishell > ");
+		printf("Line you wrote: %s\n", line);
+		if (line[0] == '\0')
+		{
+			free(line);
+			line = NULL;
+		}
+	}
+	return (0);
+}
