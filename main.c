92ecbfb91f2ecb0caf8f5af56e2b41cef4e41626
Pierre Boucher <pboucher@2F3.42angouleme.fr>
2025-01-11 17:21:34 +0100

    signals.h

--- main.c
+++ main.c
@@ -6,11 +6,25 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:23:35 by pboucher          #+#    #+#             */
-/*   Updated: 2025/01/11 15:37:37 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/01/11 16:29:17 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*ft_prompt(char *prompt)
+{
+	ft_printf("%s > ", prompt);
+	return (ft_input());	
+}
+
+void	handle_signal(int signal, siginfo_t *signals, void *context)
+{
+	(void)context;
+	(void)signals;
+	if (signal == SIGINT)
+		ft_printf("test");
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -18,18 +32,21 @@ int	main(int argc, char **argv, char **envp)
 
 	(void) argc;
 	(void) argv;
-	data.envp = envp;
-	data.line = readline("minishell > ");
-	while (line)
+	sigaction(SIGINT, &data.usr, NULL);
+	data.usr.sa_sigaction = handle_signal;
+	data.envp = ft_rlines_dup(envp);
+	data.line = readline("minishell");
+	while (data.line)
 	{
-		free(line);
-		line = readline("minishell > ");
-		printf("Line you wrote: %s\n", line);
-		if (line[0] == '\0')
+		free(data.line);
+		data.line = readline("minishell");
+		printf("Line you wrote: %s\n", data.line);
+		if (data.line[0] == '\0')
 		{
-			free(line);
-			line = NULL;
+			free(data.line);
+			data.line = NULL;
 		}
 	}
+	ft_free_rlines(&data.envp);
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:20:53 by pboucher          #+#    #+#             */
-/*   Updated: 2025/01/11 15:23:07 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/01/11 16:22:42 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "010_FT_PRINTF/ft_printf.h"
 # include <stdlib.h>
+# include <signal.h>
 
 typedef struct	s_data
 {
 	char	*line;
 	char	**envp;
+	struct sigaction	usr;
 }	t_data;
 
 #endif
