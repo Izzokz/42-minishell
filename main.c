34131d5554b9a6ecfca424f09a324ebe19992d61
Pierre Boucher <pboucher@1F3.42angouleme.fr>
2025-01-15 18:33:20 +0100

    wow

--- main.c
+++ main.c
@@ -6,12 +6,17 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:23:35 by pboucher          #+#    #+#             */
-/*   Updated: 2025/01/15 17:05:08 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/01/15 17:46:34 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	ft_exit(t_rlines command)
+{
+	if (command[0] == "exit")
+		exit(0);
+}
 
 void handler(int n)
 {
@@ -20,30 +25,6 @@ void handler(int n)
 		ft_printf("\n• minishell } ");
 	if (n == SIGQUIT)
 		return ;
-}
-
-int ft_strcmp(char *s1, char *s2)
-{
-	int i;
-	int j;
-
-	i = 0;
-	while (s1[i])
-	{
-		j = 0;
-		if (s1[i] == s2[j])
-		{
-			while (s2[j] && s1[i] == s2[j])
-			{
-				j++;
-				i++;
-			}
-			if (!s2[j])
-				return (1);
-		}
-		i++;
-	}
-	return (0);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -58,10 +39,11 @@ int	main(int argc, char **argv, char **envp)
 		signal(SIGINT, handler);
 		signal(SIGQUIT, handler);
 		data.line = readline("• minishell } ");
-		if (ft_strcmp(data.line, "exit") || !data.line)
+		if (!data.line)
 			break ;
 		data.input = ft_pipe_split(data.line);
 		ft_printf("%*.2[\n]s\n", data.input);
+		ft_exit(data.input);
 		ft_free_slines(&data.input);
 		free(data.line);
 		data.line = NULL;
