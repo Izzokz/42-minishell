6be447efac313649866ab667317737bb2750f933
Pierre Boucher <pboucher@1F3.42angouleme.fr>
2025-01-15 16:57:39 +0100

    exit works

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:23:35 by pboucher          #+#    #+#             */
-/*   Updated: 2025/01/15 14:38:45 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/01/15 16:55:38 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,16 @@ void handler(int n)
 		return ;
 }
 
+int ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+		i++;
+	return (i);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_data data;
@@ -34,7 +44,7 @@ int	main(int argc, char **argv, char **envp)
 		signal(SIGINT, handler);
 		signal(SIGQUIT, handler);
 		data.line = readline("• minishell } ");
-		if (!data.line)
+		if (ft_strcmp(data.line, "exit") || !data.line)
 			break ;
 		data.input = ft_pipe_split(data.line);
 		ft_printf("%*.2[\n]s\n", data.input);
