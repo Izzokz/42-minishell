5663515324ddb433c2cbfef149105dfc08779014
Kylian Zhen-Claverie <kzhen-cl@1B2.42angouleme.fr>
2025-01-29 17:38:55 +0100

    parsing is now more accurate

--- 000_MAIN/001_SPLIT_PARSING/ft_fix_order.c
+++ 000_MAIN/001_SPLIT_PARSING/ft_fix_order.c
@@ -25,41 +25,54 @@ static int	add_to_tmp(t_rlines *tmp, char *part)
 	return (0);
 }
 
-static int	reorder(t_rlines *part)
+static int	reorder(t_rlines *part, t_rlines *tmp, int pos)
 {
-	int			i;
-	t_rlines	tmp;
+	int	i;
 
-	tmp = ft_calloc(1, sizeof(char *));
-	if (!tmp)
-		return (ft_printf_err("Internal Error:ft_calloc(%*.)", 2));
 	i = -1;
 	while ((*part)[++i])
-		if ((*part)[i][0] == '<')
-			if (add_to_tmp(&tmp, (*part)[i]) == -1)
-				return (!!ft_free_rlines(&tmp) - 1);
-	i = -1;
-	while ((*part)[++i])
-		if ((*part)[i][0] != '<' && (*part)[i][0] != '>')
-			if (add_to_tmp(&tmp, (*part)[i]) == -1)
-				return (!!ft_free_rlines(&tmp) - 1);
-	i = -1;
-	while ((*part)[++i])
-		if ((*part)[i][0] == '>')
-			if (add_to_tmp(&tmp, (*part)[i]) == -1)
-				return (!!ft_free_rlines(&tmp) - 1);
-	ft_free_rlines(part);
-	*part = tmp;
+	{
+		if (pos == 1)
+			if ((*part)[i][0] == '<' && (*part)[i][1] == '<')
+				if (add_to_tmp(tmp, (*part)[i]) == -1)
+					return (!!ft_free_rlines(tmp) - 1);
+		if (pos == 2)
+			if ((*part)[i][0] == '<' && (*part)[i][1] != '<')
+				if (add_to_tmp(tmp, (*part)[i]) == -1)
+					return (!!ft_free_rlines(tmp) - 1);
+		if (pos == 3)
+			if ((*part)[i][0] != '<' && (*part)[i][0] != '>')
+				if (add_to_tmp(tmp, (*part)[i]) == -1)
+					return (!!ft_free_rlines(tmp) - 1);
+		if (pos == 4)
+			if ((*part)[i][0] == '>')
+				if (add_to_tmp(tmp, (*part)[i]) == -1)
+					return (!!ft_free_rlines(tmp) - 1);
+	}
 	return (0);
 }
 
 int	ft_fix_order(t_slines *input)
 {
-	int	i;
+	int			i;
+	t_rlines	tmp;
 
 	i = -1;
 	while ((*input)[++i])
-		if (reorder(&((*input)[i])) == -1)
+	{
+		tmp = ft_calloc(1, sizeof(char *));
+		if (!tmp)
+			return (ft_printf_err("Internal Error:ft_calloc(%*.)", 2));
+		if (reorder(&((*input)[i]), &tmp, 1) == -1)
 			return (-1);
+		if (reorder(&((*input)[i]), &tmp, 2) == -1)
+			return (-1);
+		if (reorder(&((*input)[i]), &tmp, 3) == -1)
+			return (-1);
+		if (reorder(&((*input)[i]), &tmp, 4) == -1)
+			return (-1);
+		ft_free_rlines(&((*input)[i]));
+			(*input)[i] = tmp;
+	}
 	return (0);
 }
