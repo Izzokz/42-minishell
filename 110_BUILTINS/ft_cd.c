9c8139c0532ad943e2351201f01717f591bc3c6d
Pierre Boucher <pboucher@1B3.42angouleme.fr>
2025-01-29 17:13:21 +0100

    fixed ft_cd (pwd and oldpwd)

--- 110_BUILTINS/ft_cd.c
+++ 110_BUILTINS/ft_cd.c
@@ -6,26 +6,52 @@
 /*   By: pboucher <pboucher@42student.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:01:50 by pboucher          #+#    #+#             */
-/*   Updated: 2025/01/28 16:35:29 by pboucher         ###   ########.fr       */
+/*   Updated: 2025/01/29 17:12:49 by pboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	ft_get_env(t_data *data, t_ints *num)
+{
+	num->i = 0;
+	num->j = 0;
+	num->k = 0;
+	while (data->envp[num->i])
+	{
+		if (!ft_strncmp(data->envp[num->i], "HOME=", 5))
+			break ;
+		num->i++;
+	}
+	while (data->envp[num->j])
+	{
+		if (!ft_strncmp(data->envp[num->j], "PWD=", 4))
+			break ;
+		num->j++;
+	}
+	while (data->envp[num->k])
+	{
+		if (!ft_strncmp(data->envp[num->k], "OLDPWD=", 7))
+			break ;
+		num->k++;
+	}
+}
+
 void	ft_cd(t_data *data, t_rlines cmd)
 {
-	char	*path;
-	int		i;
+	char		*path;
+	char		*temp;
+	t_ints		i;
 
-	i = 0;
 	(void)cmd;
-	while (data->envp[i])
-	{
-		if (!ft_strncmp(data->envp[i], "HOME=", 5))
-			break ;
-		i++;
-	}
-	path = ft_substr(data->envp[i], 5, -1);
+	ft_get_env(data, &i);
+	path = ft_substr(data->envp[i.i], 5, -1);
+	free(data->envp[i.k]);
+	temp = ft_strdup("OLDPWD=");
+	data->envp[i.k] = gnlxio_ft_strjoinfree(&temp,
+			&(char *){ft_substr(data->envp[i.j], 4, -1)});
+	free(data->envp[i.j]);
+	data->envp[i.j] = ft_strjoin("PWD=", path);
 	chdir(path);
 	free(path);
 }
