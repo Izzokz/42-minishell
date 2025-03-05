197996118e5f54e420940631df53acceca204269
Pierre Boucher <pboucher@2F3.42angouleme.fr>
2025-03-05 13:54:33 +0100

    'Fixed' echo and export LOL LMAO XDDDDDD

--- 110_BUILTINS/111_EXPORT/ft_export3.c
+++ 110_BUILTINS/111_EXPORT/ft_export3.c
@@ -14,11 +14,12 @@
 
 int	is_valid_var_name(char *name)
 {
-    if (!name || !isalpha(name[0]) && name[0] != '_')
-        return (0);
-    for (int i = 1; name[i]; i++)
+    int i;
+
+    i = 0;
+    while (name[++i] && name[i] != '=')
     {
-        if (!isalnum(name[i]) && name[i] != '_')
+        if (!ft_isalnum(name[i]) && name[i] != '_' && name[i] != '+')
             return (0);
     }
     return (1);
